7c4d3b85e90e13b555c7e4372dbbc02e5db85142
ig0r74 <ig0r74@yandex.ru>
2018-12-14 18:52:16 +0300

    Добавлена возможность использовать несколько форм на странице

--- core/components/recaptchav3/elements/snippets/recaptchav3_html.php
+++ core/components/recaptchav3/elements/snippets/recaptchav3_html.php
@@ -2,29 +2,32 @@
 $public = $modx->getOption('formit.recaptcha_public_key');
 $action = $action ?: 'ajaxform';
 
-$modx->regClientScript('
-    <script>
-        function grecaptchaExecute() {
-            grecaptcha.execute("' . $public . '", { action: "' . $action . '" })
-            .then(function(token) {
-                document.getElementById("g-recaptcha-response").value=token;
-            });
-        }
-        grecaptcha.ready(function() {
-            grecaptchaExecute();
-        });
-        $(document).on("af_complete", function(event, response) {
-            if (response.success) {
-                grecaptchaExecute();
+if (!$modx->getPlaceholder('rcv3_initialized')) {
+    $modx->regClientStartupScript('<script src="https://www.google.com/recaptcha/api.js?render=' . $public . '"></script>');
+    $modx->regClientScript('
+        <script>
+            function grecaptchaExecute() {
+                grecaptcha.execute("' . $public . '", { action: "' . $action . '" })
+                .then(function(token) {
+                    $("[name =\'g-recaptcha-response\']").val(token);
+                });
             }
-        });
-    </script>
-', true);
+            grecaptcha.ready(function() {
+                grecaptchaExecute();
+            });
+            $(document).on("af_complete", function(event, response) {
+                if (response.success) {
+                    grecaptchaExecute();
+                }
+            });
+        </script>
+    ', true);
+    $modx->setPlaceholder('rcv3_initialized', 1);
+}
 
 $output = '
-    <script src="https://www.google.com/recaptcha/api.js?render=' . $public . '"></script>
     <span class="error_g-recaptcha-response error error_message">' . $error . '</span>
-    <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response">
+    <input type="hidden" name="g-recaptcha-response">
 ';
 
 return $output;
